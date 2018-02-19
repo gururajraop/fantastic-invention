4a7d6cae71787adbc6b334c8efa48e4f59a355d3
Gururaj Rao <gururajraop@gmail.com>
2018-02-19 11:57:10 +0100

    Add visualization for color space results

---
+++ colorspace/rgb2opponent.m
@@ -0,0 +1,7 @@
+function [output_image] = rgb2opponent(input_image)
+% converts an RGB image into opponent color space
+
+output_image = input_image;
+
+end
+
---
+++ colorspace/visualize.m
@@ -0,0 +1,23 @@
+function visualize(input_image, color_space)
+% Plot the results of converted color space
+figure
+
+% The output image
+subplot(2,2,1);
+imshow(input_image);
+title(color_space(1, 1));
+
+subplot(2,2,2);
+imshow(input_image(:, :, 1))
+title(color_space(1, 2))
+
+subplot(2,2,3);
+imshow(input_image(:, :, 2))
+title(color_space(1, 3))
+
+subplot(2,2,4);
+imshow(input_image(:, :, 3))
+title(color_space(1, 4))
+
+end
+
