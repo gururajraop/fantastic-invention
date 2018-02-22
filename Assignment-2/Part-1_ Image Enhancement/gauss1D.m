f0595567661f8a067e34e9290ae82bf66c502692
Gururaj Rao <gururajraop@gmail.com>
2018-02-22 17:03:59 +0100

    Add 1D gaussian filter code

--- Assignment-2/Part-1_ Image Enhancement/gauss1D.m
+++ Assignment-2/Part-1_ Image Enhancement/gauss1D.m
@@ -4,4 +4,11 @@ function G = gauss1D( sigma , kernel_size )
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
     %% solution
+    
+    size = floor(kernel_size / 2);
+    X = -size:size;
+    disp(X);
+    
+    G = (1 / (sigma * sqrt(2 * pi))) * exp(-((X .^ 2) / (2 * sigma * sigma)));
+    G = G ./ sum(G);
 end
---
+++ Assignment-2/Part-1_ Image Enhancement/main.m
@@ -0,0 +1,5 @@
+clear
+clc
+close all
+
+disp(gauss1D(2, 5));
