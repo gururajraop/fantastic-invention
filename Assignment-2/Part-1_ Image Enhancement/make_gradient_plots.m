348174f820253c437fa230e552b611be412680e3
Freek Boutkan <freek.boutkan@gmail.com>
2018-02-27 16:08:43 +0100

    Minor fixes

--- Assignment-2/Part-1_ Image Enhancement/make_gradient_plots.m
+++ Assignment-2/Part-1_ Image Enhancement/make_gradient_plots.m
@@ -1,7 +1,9 @@
+close all
+
 im = im2double(imread('images/image2.jpg'));
 I = im;
 
-%% Matlab implementation to compare to
+%% Matlab implementation to compare to, not used in report
 [Gx, Gy] = imgradientxy(I);
 [Gmag, Gdir] = imgradient(Gx, Gy);
 
