ddac3ea61078cfa3bd254106aa10dfa7850d7bc2
Gururaj Rao <gururajraop@gmail.com>
2018-03-02 11:52:24 +0100

    Add the skeleton structure for the assignment

---
+++ Assignment-3/lucas_kanade.m
@@ -0,0 +1,4 @@
+function [] = lucas_kanade()
+
+end
+
---
+++ Assignment-3/main.m
@@ -0,0 +1,12 @@
+clear
+clc
+close all
+
+% Harris-Corner Detector
+harris_corner_detector();
+
+% Optical Flow with Lucas-Kanade Algorithm
+lucas_kanade();
+
+% Feature Tracking
+tracking();
