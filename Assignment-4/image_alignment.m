19089e29d74470e9c8fbff1451d358b17fdeec77
Gururaja Rao P <gururajraop@gmail.com>
2018-03-12 16:40:49 +0100

    Interpolation logic

--- Assignment-4/image_alignment.m
+++ Assignment-4/image_alignment.m
@@ -3,12 +3,10 @@ function [] = image_alignment()
     image2 = im2single(imread('./boat2.pgm'));
 
     [matches, f1, f2, ~, ~] = keypoint_matching(image1, image2);
-    size(matches)
-    size(f1)
-    size(f2)
 
     [f1_new, t_new, transformation_1] = RANSAC(matches, f1, f2);
-    plot_matching_points(image1, image2, t_new, f1, f1_new);
+%     plot_matching_points(image1, image2, t_new, f1, f1_new);
+    interpolate(image1, transformation_1);
 
 %     m = [matches(2, :); matches(1, :)];
 %     [f2_new, t_new, transformation_2] = RANSAC(m, f2, f1);
---
+++ Assignment-4/interpolate.m
@@ -0,0 +1,30 @@
+function [] = interpolate(image, transformation)
+[h, w] = size(image);
+
+m = reshape(transformation(1:4), 2, 2);
+t = reshape(transformation(5:6), 2, 1);
+corners = round([m * [1; 1] + t, m * [1; w] + t, m * [h; 1] + t, m * [h; w] + t]);
+h_extra = -min(corners(1,:)) + 1;
+w_extra = -min(corners(2,:)) + 1;
+h_new = max(corners(1,:)) - min(corners(1,:)) + 1;
+w_new = max(corners(2,:)) - min(corners(2,:)) + 1;
+
+image_transformed = zeros(h_new, w_new);
+
+for x = 1:h
+    for y = 1:w
+        coor = round(m * [x;y] + t) + [h_extra; w_extra];
+        image_transformed(coor(1), coor(2)) = image(x, y);
+    end
+end
+
+T_m = [m' t; 0 0 1]';
+T = maketform('affine', T_m);
+img_matlab = imtransform(image, T);
+
+figure()
+subplot(1,3,1), imshow(image); title('Original Image');
+subplot(1,3,2), imshow(image_transformed); title('Transformed Image');
+subplot(1,3,3), imshow(img_matlab); title('Matlab tranformation');
+
+end
