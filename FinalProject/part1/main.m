fda5a616d8cbbebd39843741b136afc42682d622
Freek Boutkan <freek.boutkan@gmail.com>
2018-03-19 15:58:45 +0100

    Save functionality

--- FinalProject/part1/main.m
+++ FinalProject/part1/main.m
@@ -4,6 +4,7 @@ close all
 
 %% Read the input images and process them for various setup
 num_images = 10;
+k = 400;
 file_names = get_file_names(num_images);
 
 % data_processing(file_names);
@@ -12,7 +13,8 @@ file_names = get_file_names(num_images);
 features = feature_extraction(file_names);
 
 %% Build visual vocabulory
-vocab = build_visual_vocab(features, 400);
+vocab = build_visual_vocab(features, k);
+% save(sprintf('vocab/%d_images_%d_vocabsize.mat', num_images, k), 'vocab')
 
 %% Quantize the features using the visual vocabulory
 quantization();
