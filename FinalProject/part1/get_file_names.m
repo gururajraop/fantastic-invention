20cd15b02a6e72239526197a54c83ea88459969b
Gururaja Rao P <gururajraop@gmail.com>
2018-03-20 15:24:17 +0100

    Setup to build all vocab

--- FinalProject/part1/get_file_names.m
+++ FinalProject/part1/get_file_names.m
@@ -1,4 +1,4 @@
-function [selected_files] = get_file_names(n)
+function [selected_files, unselected_files] = get_file_names(n)
 %% User defined parameters
 classes = {'airplanes', 'cars', 'faces', 'motorbikes'};
 path = './Caltech4/';
@@ -14,12 +14,19 @@ end
 
 %% Read the images
 selected_files = strings(4, n);
+unselected_files = strings(4, 500-n);
 for class=1:4
     perm = randperm(size(image_list{class}{1}, 1));
     select = perm(1:n);
     j = 1;
     for i=select
         selected_files(class, j) = image_list{class}{1}{i};
+        j = j + 1;
+    end
+    unselect = perm(n+1:end);
+    j = 1;
+    for i=unselect
+        unselected_files(class, j) = image_list{class}{1}{i};
         j = j + 1;
     end
 end
--- FinalProject/part1/main.m
+++ FinalProject/part1/main.m
@@ -3,24 +3,27 @@ clc
 close all
 
 %% User defined parameters
-num_images = 10;            % number of images used in vocabulary
+num_images = 250;           % number of images used in vocabulary
 k = 400;                    % k value for k-means clustering
 sift_type = 'normal';       % the sift type
                             % 'normal' 'dense'
 color_space = 'gray';       % color space to be used
                             % 'gray' 'RGB' 'rgb' 'opponent'
                             
-
-%% Read the input images and process them for various setup
-file_names = get_file_names(num_images);
-
-%% Feature extraction and description
-features = feature_extraction(file_names, sift_type, color_space);
-
-%% Build visual vocabulory
-vocab = build_visual_vocab(features, k);
-% save(sprintf('vocab/%d_images_%d_vocabsize.mat', num_images, k), 'vocab')
-
+%% Build all types of vocabulary and save them
+build_all_vocab();
+                            
+                            
+% %% Read the input images and process them for various setup
+% file_names = get_file_names(num_images);
+% 
+% %% Feature extraction and description
+% features = feature_extraction(file_names, sift_type, color_space);
+% 
+% %% Build visual vocabulory
+% vocab = build_visual_vocab(features, k);
+% save(sprintf('vocab/%d_images_%d_vocabsize_%s_%s.mat', num_images, k, sift_type, color_space), 'vocab')
+% 
 
 %% Quantize the features using the visual vocabulory
 % quantization(vocab, image, sift_type, color_space);
