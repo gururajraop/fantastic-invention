4cb52875699d5235ee66050de89553dd28b17dfa
Freek Boutkan <freek.boutkan@gmail.com>
2018-03-27 00:22:45 +0200

    New vocab

--- FinalProject/part1/startup.m
+++ FinalProject/part1/startup.m
@@ -8,4 +8,4 @@ run(VLFEAT_ROOT);
 matconvpath  = "C:\Users\WilIk\Desktop\matconvnet-1.0-beta25\matlab\vl_setupnn.m";
 run(matconvpath)
 
-run(".\part2\liblinear-2.1\matlab\make.m")
+run("..\part2\liblinear-2.1\matlab\make.m")
