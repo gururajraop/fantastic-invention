7456a2c698f93c3670687ab4293907cac84bb854
Gururaja Rao P <gururajraop@gmail.com>
2018-03-26 13:54:51 +0200

    Add training using pretrained vocab

--- FinalProject/part1/build_visual_vocab.m
+++ FinalProject/part1/build_visual_vocab.m
@@ -1,6 +1,6 @@
 function [vocab] = build_visual_vocab(features, k, maxiter, vl_sift)
     if vl_sift
-        [~, vocab] = vl_kmeans(features', k, 'MaxNumIterations', maxiter);   
+        [vocab, ~] = vl_kmeans(features', k, 'MaxNumIterations', maxiter);   
     else
         [~, vocab] = kmeans(features, k, 'Start', 'cluster', 'maxiter', maxiter);   
     end
--- FinalProject/part1/main.m
+++ FinalProject/part1/main.m
@@ -3,28 +3,29 @@ clc
 close all
 
 %% User defined parameters
-num_images = 10;           % number of images used in vocabulary
+num_images = 250;           % number of images used in vocabulary
 k = 400;                    % k value for k-means clustering
-sift_type = 'normal';       % the sift type
+sift_type = 'dense';       % the sift type
                             % 'normal' 'dense'
 color_space = 'gray';       % color space to be used
                             % 'gray' 'RGB' 'rgb' 'opponent'
                             
 %% Build all types of vocabulary and save them
-build_all_vocab();
-                            
-                            
-% %% Read the input images and process them for various setup
+% build_all_vocab();
+
+%% Read the input images and process them for various setup
 % [file_names, remaining] = get_file_names(num_images);
-% 
-% %% Feature extraction and description
+
+%% Feature extraction and description
 % features = feature_extraction(file_names, sift_type, color_space);
-% 
-% %% Build visual vocabulory
+
+%% Build visual vocabulory
 % vocab = build_visual_vocab(features, k);
-% 
-% %% Image representation
-% % representation();
-% 
-% %% Classification
-% classification(remaining, vocab, sift_type, color_space);
+
+%% Image representation
+% representation();
+
+%% Classification
+load(sprintf('vocab/%d_images_%d_vocabsize_%s_%s.mat', num_images, k, sift_type, color_space), 'vocab');
+load(sprintf('vocab/unselected-%d_images_%s_%s.mat', num_images, sift_type, color_space), 'unselected');
+classification(unselected, vocab, sift_type, color_space);
